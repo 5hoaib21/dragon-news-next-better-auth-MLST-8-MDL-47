e80943d0534c0ca06a59a1e8f9f0f70332905a74
MD ABDUR RAHMAN SHOAIB <mdabdurrahmanshoaib@gmail.com>
2026-05-02 18:47:11 +0600

    Social Sign-In (Google And Github)

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -7,21 +7,27 @@ import { mongodbAdapter } from "better-auth/adapters/mongodb";
 const client = new MongoClient(process.env.MONGO_DB);
 const db = client.db('dragon-news');
 
-<<<<<<< HEAD
-=======
 
->>>>>>> b31291259ec018a62472b46dad0e718f8eba80b7
 
 export const auth = betterAuth({
   database: mongodbAdapter(db, {
     // Optional: if you don't provide a client, database transactions won't be enabled.
     client
   }),
-<<<<<<< HEAD
+
   emailAndPassword: { 
     enabled: true, 
   },
+  socialProviders: {
+        google: { 
+            clientId: process.env.GOOGLE_CLIENT_ID ,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+        }, 
+        github: { 
+            clientId: process.env.GITHUB_CLIENT_ID, 
+            clientSecret: process.env.GITHUB_CLIENT_SECRET, 
+        },
+    },
 });
-=======
-});
->>>>>>> b31291259ec018a62472b46dad0e718f8eba80b7
+
+
